9fe302c12f3e83a4b9ede2c8de6d58afca681910
Mastermind730 <sourav.97032@gmail.com>
2024-02-17 23:51:16 +0530

    added signup page and backend code for login and signup

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -1,20 +1,24 @@
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-    <a
+    <Link
       href="#"
       className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
     >
-      <img
+      <Image
         className="w-8 h-8 mr-2"
-        src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+        src={"/images/Logo.png"}
         alt="logo"
+        width={200}
+        height={200}
       />
-      Flowbite
-    </a>
+      Volununity
+    </Link>
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
@@ -88,12 +92,12 @@ const page = () => {
           </button>
           <p className="text-sm font-light text-gray-500 dark:text-gray-400">
             Don’t have an account yet?{" "}
-            <a
-              href="#"
+            <Link
+              href="/signup"
               className="font-medium text-primary-600 hover:underline dark:text-primary-500"
             >
               Sign up
-            </a>
+            </Link>
           </p>
         </form>
       </div>
---
+++ src/lib/dbconnect.js
@@ -0,0 +1,2 @@
+const {username,password}=process.env;
+export const connectionSrt=`mongodb+srv://beastsourav09:${password}@cluster0.rrohtve.mongodb.net/Volununity?retryWrites=true&w=majority`;
